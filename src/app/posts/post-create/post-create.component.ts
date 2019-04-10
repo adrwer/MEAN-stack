2225d861d92815a60bc0639f1ebe9c9af84242c1
Adrian Werimo <adrwer7@gmail.com>
2019-04-10 13:09:33 +0300

    Adding a header, outputting posts, using structural directives, property and event binding and one cerrection to the textarea element

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component ({
   selector: 'app-post-create',
@@ -6,13 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent {
-  enteredValue = '';
-  newPost = 'No Content';
+  // enteredValue = '';
+  // newPost = 'No Content';
+  enteredTitle = '';
+  enteredContent = '';
+  @Output() postCreated = new EventEmitter();
 
   /* onAddPost(postInput: HTMLTextAreaElement) {
     this.newPost = postInput.value;
   } */
   onAddPost() {
-    this.newPost = this.enteredValue;
+    // this.newPost = this.enteredValue;
+    const post = {title: this.enteredTitle, content: this.enteredContent};
+    this.postCreated.emit(post);
   }
 }
