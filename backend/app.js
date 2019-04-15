f9f818ca156c241642c4b7116bfbcaa6cdd6e43f
Adrian Werimo <adrwer7@gmail.com>
2019-04-15 13:39:12 +0300

    transforming response data, deleting data and manipulating frontend after updating db

--- backend/app.js
+++ backend/app.js
@@ -33,20 +33,28 @@ app.post("/api/posts", (req, res, next) => {
     title: req.body.title,
     content: req.body.content
   });
-  post.save();
-  console.log(post);
-  res.status(201).json({
-    message: "Posts added successfully"
+  post.save().then(result => {
+    res.status(201).json({
+      message: "Posts added successfully",
+      postId: result._id
+    });
   });
 });
 
-app.use("/api/posts",(req, res, next) => {
+app.get("/api/posts",(req, res, next) => {
   Post.find()
   .then(documents => {
     res.status(200).json({
       message: "Posts fetched successfully",
       posts: documents
     });
+  });
+});
+
+app.delete("/api/posts/:id", (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id }).then(result => {
+    console.log(result);
+    res.status(200).json({message: "Post deleted successfully!"});
   });
 });
 
