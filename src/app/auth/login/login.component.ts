876af20ac3942cfbb115d01a313cb222f60cba8e
Adrian Werimo <adrwer7@gmail.com>
2020-01-21 11:31:58 +0300

    fixed ngForms bug in login and signup components

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   templateUrl: './login.component.html',
